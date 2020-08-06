43412606a4fdeb615427d36f421c60b8875080d4
Guillermo G. Kelly S <malak_ai@hotmail.com>
2020-08-05 22:21:43 -0500

    Grunt Part Two

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,9 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     /* Automatically load required Grunt tasks. */
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     /* Define the configuration for all the tasks. */
     grunt.initConfig ({
@@ -31,7 +33,7 @@ module.exports = function (grunt) {
                         'js/*.js'
                     ]
                 }, /* WATCH THE COMMAS. */
-                option: {
+                options: {
                     watchTask: true,
                     server: {
                         baseDir: './'
@@ -69,9 +71,79 @@ module.exports = function (grunt) {
                         expand:true,
                         dot: true,
                         cwd: './',
-                        src: ['img/*.{png,jpg,gif'],
+                        src: ['img/*.{png,jpg,gif}'],
                         dest: 'dist/'
                     }]
+                }
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify'],
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8', 
+                algorithm: 'md5',
+                length: 20,
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html'
                 }
             }
         }
@@ -79,4 +151,16 @@ module.exports = function (grunt) {
 
     grunt.registerTask('css',['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 };
